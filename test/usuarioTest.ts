b593e8a278dfc1a7a1140b0a2ae229320dc270ea
AguirreAgustin <ardagustin@gmail.com>
2019-05-25 18:55:56 -0300

    Clase Usuario con Test de Agregar Notificacion

--- test/usuarioTest.ts
+++ test/usuarioTest.ts
@@ -4,13 +4,28 @@ import { expect } from 'chai';
 
 describe('Usuario', () => {
  
-    it('Test Constructor', () => {
+    it('Test Constructor Usuario', () => {
     
         let usuario = new Usuario('Agustín Aguirre Ruíz Díaz',41038330);
 
         expect(usuario.getNombre()).to.equal("Agustín Aguirre Ruíz Díaz");
 
         expect(usuario.getDni()).to.equal(41038330);
+
+
+    }); 
+    it('Test Agregar Notificacion Usuario', () => {
+    
+        let usuario = new Usuario('Agustín Aguirre Ruíz Díaz',41038330);
+        let notificacion = new Notificacion('','',123,'','');
+
+        expect(usuario.getNotificaciones().length).to.equal(0);
+
+        usuario.agregarNotificacion(notificacion);
+
+        expect(usuario.getNotificaciones().length).to.equal(1);
+        
+        
 
 
     }); 
