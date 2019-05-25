bc036427f59aa1e67f775ef2ff069f4997a756e4
CristianSaraceni <torry.90.03@gmail.com>
2019-05-25 19:19:06 -0300

    Refactorizar test Constructor

--- test/notificacionTest.ts
+++ test/notificacionTest.ts
@@ -1,23 +1,23 @@
 import { Notificacion } from '../src/Notificacion';
 import { expect } from 'chai';
+import moment = require("moment");
+
 
 describe('Notificacion', () => {
  
-    it('Test ID', () => {
-    
-        let notificacion = new Notificacion('','',123,'','');
-       expect(notificacion.getId()).to.equal(123);
-
-    }); 
-});
-
-describe('Notificacion', () => {
- 
-    it('Test Visto', () => {
-    
-        let notificacion = new Notificacion('','',123,'','');
-       notificacion.cambiarVisto;
+    it('Test Constructor Notificacion', () => {
+        var fecha = moment('2016-01-01');
+        let notificacion = new Notificacion('Titulo','Descripcion',12345,fecha,'Cristian');
+        
+        expect(notificacion.getTitulo()).to.equal('Titulo');
+        expect(notificacion.getDescripcion()).to.equal('Descripcion');
+        expect(notificacion.getId()).to.equal(12345);
+        expect(notificacion.getFecha()).to.equal(fecha);
+        expect(notificacion.getRemitente()).to.equal('Cristian');
+        notificacion.cambiarVisto;
         expect(notificacion.getVisto()).to.equal(false);
 
     }); 
+        
 });
+
