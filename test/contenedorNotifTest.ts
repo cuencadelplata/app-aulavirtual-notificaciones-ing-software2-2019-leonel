a0176a60d6cedb4860243ed64d7fcdfa51309246
Leonel <lolo042@hotmail.com>
2019-05-26 13:55:41 -0300

    getInstance declarada como variable global en  ContenederoNotifTest

--- test/contenedorNotifTest.ts
+++ test/contenedorNotifTest.ts
@@ -4,10 +4,10 @@ import { Notificacion } from '../src/Notificacion';
 import moment = require("moment");
 
 describe('Almacenar notificaciones', () => {
+    var contenedorNotificacion = ContenedorNotif.getInstance();
  
     it('Cantidad de notificaciones debe ser igual a 1', () => {
         var fecha = moment('2016-01-01');
-        let contenedorNotificacion = ContenedorNotif.getInstance();
         let notificacion = new Notificacion("esto es un titulo", "esto es una descripcion", 1, fecha,"soy el remitente");
         contenedorNotificacion.agregarNotificacion(notificacion);
         expect(contenedorNotificacion.getNotificaciones().length).to.equal(6);
@@ -16,7 +16,6 @@ describe('Almacenar notificaciones', () => {
 
     it('Cantidad de notificaciones no debe ser igual a 1', () => {
         var fecha = moment('2016-01-01');
-        let contenedorNotificacion = ContenedorNotif.getInstance();
         let notificacion = new Notificacion("esto es un titulo", "esto es una descripcion", 1, fecha,"soy el remitente");
         contenedorNotificacion.agregarNotificacion(notificacion);
         expect(contenedorNotificacion.getNotificaciones().length).to.not.equal(2);
