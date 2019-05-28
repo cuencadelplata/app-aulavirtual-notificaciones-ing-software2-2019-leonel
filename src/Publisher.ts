d7729aa23f18cb210b1e0b84f8f339684416534f
JcBlan <blancojulic@gmail.com>
2019-05-28 17:19:50 -0300

    Añadido test Enviar Notificaciones

--- src/Publisher.ts
+++ src/Publisher.ts
@@ -1,3 +1,7 @@
+import { parsear } from "../src/Parser";
+import { Canal } from  "../src/Canal";
+import { Notificacion } from "../src/Notificacion";
+import moment = require("moment");
 
 export class Publisher
 {
@@ -5,7 +9,7 @@ export class Publisher
 
     constructor(){
 
-      this.ultimoMensaje = 0
+      this.ultimoMensaje = 0;
 
     }
 
@@ -19,5 +23,23 @@ export class Publisher
       return this.ultimoMensaje;
     }
 
-    
+    public async crearYenviarNotificacion( canal : Canal)
+    {
+        let datos = await parsear("http://virtualucp.edu.ar/cursos/rss/file.php?file=/154678/a1329bd266c85f1c5aa024a6ba396df5/mod_forum/11489/rss.xml");
+        
+        datos.items.slice().reverse().forEach(mensaje => {
+
+          var nombre = mensaje.content.slice(4, mensaje.content.indexOf("."));
+          var descripcion = mensaje.content.slice(mensaje.content.indexOf("<p><p>")+6, mensaje.content.indexOf("</p></p>"));
+          var id =  Number(mensaje.link.slice(mensaje.link.length - 5));
+          var fecha = mensaje.pubDate.slice(5);
+
+          if(this.getUltimoMensaje() < id)
+          {
+            var noti = new Notificacion(mensaje.title, descripcion, id, moment(fecha), nombre );
+            canal.repartirNotificacion(noti);
+            this.setUltimoMensaje(id);
+          }
+        });
+    }
 }
--- test/publisherTest.ts
+++ test/publisherTest.ts
@@ -1,9 +1,25 @@
 import { Publisher } from "../src/Publisher";
 import { expect } from 'chai';
+import { Canal } from "../src/Canal";
+import { Usuario } from "../src/Usuario";
 
 describe('Crear Publisher', () => {
     it('Creacion correcta', () => {
         var publisher = new Publisher();
         expect(publisher).to.not.equal(null);
+    });
+});
+
+describe("Leer RSS y enviar notificaciones", () => {
+    it("Enviar notificaciones", ()=> {
+        var publisher = new Publisher();
+        var canal = new Canal();
+        var usuario = new Usuario("Usuario", 12345567);
+        canal.subscribirse(usuario);
+        
+        (async () => {
+            publisher.crearYenviarNotificacion(canal);
+            expect(usuario.getNotificaciones().length).to.be.greaterThan(0);
+        }) ();
     });
 });
