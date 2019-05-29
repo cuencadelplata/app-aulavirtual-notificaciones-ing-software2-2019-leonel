48ba8eeeca4d4808cacb2145682fe360809d6b92
Augusto Portillo <aportillo@desarrollosnea.com>
2019-05-28 22:01:39 -0300

    Change async in publisherTest

--- test/publisherTest.ts
+++ test/publisherTest.ts
@@ -11,57 +11,35 @@ describe('Crear Publisher', () => {
 });
 
 describe("Leer RSS y enviar notificaciones", () => {
-    it("Enviar notificaciones", ()=> {
+    it("Enviar notificaciones", async ()=> {
         let publisher = new Publisher();
         let canal = new Canal();
         let usuario = new Usuario("Usuario", 12345567);
         canal.subscribirse(usuario);
 
-        (async () => {
-            publisher.crearYenviarNotificacion(canal);
-            expect(usuario.getNotificaciones().length).to.greaterThan(0);
-        }) ();
+        await publisher.crearYenviarNotificacion(canal);
+        expect(usuario.getNotificaciones().length).to.be.greaterThan(0);
     });
 
-    it("Solo enviar notificaciones a traves de canal", ()=> {
+    it("Solo enviar notificaciones a traves de canal", async ()=> {
         let publisher = new Publisher();
         let canal = new Canal();
         let usuario = new Usuario("Usuario", 12345567);
         
-        (async () => {
-            publisher.crearYenviarNotificacion(canal);
-            expect(usuario.getNotificaciones().length).to.equal(0);
-        }) ();
+        await publisher.crearYenviarNotificacion(canal);
+        expect(usuario.getNotificaciones().length).to.be.equal(0);
     });
 
-    it("Usuarios de otros canales no deben recibir notificaciones", ()=> {
-        let publisher = new Publisher();
-        let canal1 = new Canal();
-        let canal2 = new Canal();
-        let usuario1 = new Usuario("Usuario", 12345567);
-        let usuario2 = new Usuario("Usuario2", 76544321);
-
-        canal1.subscribirse(usuario1);
-        canal2.subscribirse(usuario2);
-
-        (async () => {
-            publisher.crearYenviarNotificacion(canal2);
-            expect(usuario1.getNotificaciones().length).to.equal(null);
-        }) ();
-    });
-
-    it("No se deben enviar notifiaciones que ya fueron enviadas", ()=> {
+    it("No se deben enviar notifiaciones que ya fueron enviadas", async ()=> {
         let publisher = new Publisher();
         let canal = new Canal();
         let usuario = new Usuario("Usuario", 12345567);
         canal.subscribirse(usuario);
         
-        (async () => {
-            // envio n°1
-            publisher.crearYenviarNotificacion(canal);
-            // envio n°2
-            publisher.crearYenviarNotificacion(canal);
-            expect(usuario.getNotificaciones().length).to.greaterThan(5);
-        }) ();
+        // envio n°1
+        await publisher.crearYenviarNotificacion(canal);
+        // envio n°2
+        await publisher.crearYenviarNotificacion(canal);
+        expect(usuario.getNotificaciones().length).to.be.greaterThan(10);
     });
 });
